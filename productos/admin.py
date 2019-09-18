80bb2445c11979fedf18b455d086fcd25e1f45d7
jorgemv1986 <jorgemv1986@gmail.com>
2019-09-18 01:34:30 -0500

    heroku ++++

--- productos/admin.py
+++ productos/admin.py
@@ -1,16 +1,36 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from productos.models import Presentacion, Producto
 
 
-@admin.register(Presentacion)
-class PresentacionAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
+class ProductoResource(resources.ModelResource):
+    class Meta:
+        model = Producto
 
 
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
+class ProductoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre', 'descripcion']
     list_display = (
-    'lote', 'presentacion', 'nombre', 'descripcion', 'fecha_expiracion', 'fecha_produccion', 'tipo', 'precio_compra',
-    'precio_venta', 'stock')
-    search_fields = ('nombre', 'descripcion')
+        'lote', 'presentacion', 'nombre', 'descripcion', 'fecha_expiracion', 'fecha_produccion', 'tipo',
+        'precio_compra',
+        'precio_venta', 'stock',)
+    resource_class = ProductoResource
+
+
+admin.site.register(Producto, ProductoAdmin)
+
+
+class PresentacionResource(resources.ModelResource):
+    class Meta:
+        model = Presentacion
+
+
+class PresentacionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre']
+    list_display = ('nombre',)
+    resource_class = PresentacionResource
+
+
+admin.site.register(Presentacion, PresentacionAdmin)
